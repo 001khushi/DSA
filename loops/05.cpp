d6f932b1ab48fb9084374dfc21cc4ac7875a02af
Khushi Saxena <aachalsaxena001@.gmail.com>
2022-01-12 00:06:09 +0530

    Practice

---
+++ loops/05.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=5;
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=n-i;j>0;j--)
+        {
+            cout<<" ";
+        }
+        for(int j=i;j<=n;j++)
+        {
+            cout<<"* ";
+        }
+        
+        cout<<endl;
+
+    }
+}
